857d56a67b23efe2a4d071d8680b26708d3ac339
Sacha Sedriks <sachased@gmail.com>
2020-09-18 08:45:39 -0700

    Update so all Drum Machine titles are in input text fields

--- src/components/DrumMachine/Header.js
+++ src/components/DrumMachine/Header.js
@@ -8,26 +8,20 @@ export default function Header (props) {
             {/* {!!track.title.length &&
                 <h1>{track.title}</h1>} */}
 
-            {(track.id !== 0)
-                ? 
-                <h1>{track.title}</h1>
-                :
-                // TODO: Enable for all /track/:trackId's owned by signed-in user
-                <form className="title-form">
-                    <div>
-                        <label>
-                            Track Title
-                            <input 
-                                type="text"
-                                name="title"
-                                id="title"
-                                placeholder="Untitled" 
-                                value={track.title}
-                                onChange={e => titleChange(e, track.id)}/>
-                        </label>
-                    </div>
-                </form>
-            }
+            <form className="title-form">
+                <div>
+                    <label>
+                        Track Title
+                        <input 
+                            type="text"
+                            name="title"
+                            id="title"
+                            placeholder="Untitled" 
+                            value={track.title}
+                            onChange={e => titleChange(e, track.id)}/>
+                    </label>
+                </div>
+            </form>
 
         </header>
     )
